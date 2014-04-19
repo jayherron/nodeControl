43b64bca24c1b84c31c4ce2be2d97ef33874a10b
vagrant <vagrant@precise32.(none)>
2014-04-19 18:29:48 +0000

    heroku

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-var express = require ('express');
+var express = require ('express')
+, path = require('path');
 
 var app = express()
-var port = 8000;
-app.listen(port);
+
+require('./routes')(app);
+
+
+
+app.use(express.static(path.normalize(__dirname) + '/public'))
+
+
+app.use(express.errorHandler());
+
+app.configure(function() {
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+})
+
+
+
+app.set('views', path.normalize(__dirname) + '/public');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderfile);
+
+
+
+
+var port = Number(process.env.PORT || 8000);
+app.listen(8000l);
 console.log("Express app listening on port" + port);
-
-app.get('/', function(req, res){
-	res.send('Hello'); 
-
-
-
-}); 
